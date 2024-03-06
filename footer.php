1ccf2426109c9eb484831ac2c0c4a398a3391e62
eunji <dmswl7954@gmail.com>
2024-03-06 13:31:08 +0900

    링크설정 완료

---
+++ footer.php
@@ -0,0 +1,181 @@
+    <footer>
+        <div class="footer_bg">
+            <div class="container">            
+                <div class="first">
+                    <div class="harf_left">
+                        <p>공지</p>
+                        <a href="#">[공지] 파운데이션 팩토리 서비스 변경 안내</a>
+                        <p><a href="#">더보기<em>+</em></a></p>
+                    </div>
+                    <!-- 데스크탑 버전에만 있는 footer nav부분 -->
+                    <div class="harf_right">
+                        <div class="family_site">
+                            <a href="#">
+                                <span>Family Site</span>
+                                <img src="./images/icon/arrow-footer-select.svg" alt="" class="btn_arw"></a>
+                            <div class="btn_lnb1">
+                                <a href="#">ETUDE GLOBAL MALL</a>
+                                <a href="#">JAPAN</a>
+                            </div>
+                        </div>
+                        <div class="site_map">
+                            <a href="#">
+                                <span>Site Map</span>
+                                <img src="./images/icon/arrow-footer-select.svg" alt="" class="btn_arw"></a>
+                            <ul class="btn_lnb2">
+                                <li class="left">
+                                    <ul class="title">
+                                        <li><a href="./brand_earth.html">BRAND</a></li>
+                                    </ul>
+                                    <ul class="sub">
+                                        <li><a href="#">About ETUDE</a></li>
+                                        <li><a href="#">뷰티즌</a></li>
+                                        <li><a href="./brand_earth.html">지구를 위한 활동</a></li>
+                                    </ul>
+                                    <ul class="title">
+                                        <li><a href="./product.html">PRODUCT</a></li>
+                                    </ul>
+                                    <ul class="sub">
+                                        <li><a href="./cate_new.html">전체보기</a></li>
+                                        <li><a href="./cate_new.html">신제품</a></li>
+                                        <li><a href="./cate_new.html">카테고리</a>
+                                            <ul>
+                                                <li>
+                                                    <a href="./product.html">아이</a>
+                                                    <a href="./product.html">립</a>
+                                                    <a href="./product.html">페이스</a>
+                                                    <a href="./product.html">네일</a>
+                                                    <a href="./product.html">스킨케어</a>
+                                                </li>
+                                                <li>
+                                                    <a href="./product.html">마스크·팩</a>
+                                                    <a href="./product.html">클렌저</a>
+                                                    <a href="./product.html">바디</a>
+                                                    <a href="./product.html">헤어</a>
+                                                    <a href="./product.html">툴</a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                    <ul class="title">
+                                        <li><a href="./makeuplook.html">MAKEUP LOOK</a></li>
+                                    </ul>
+                                    <ul class="sub">
+                                        <li><a href="./makeuplook.html">메이크업룩</a></li>
+                                    </ul>
+                                    <ul class="title">
+                                        <li><a href="./factory.html">FOUNDATION FACTORY</a></li>
+                                    </ul>
+                                    <ul class="sub">
+                                        <li><a href="#">서비스안내</a></li>
+                                        <li><a href="#">예약하기</a></li>
+                                    </ul>
+                                </li>
+                                <li class="right">
+                                    <ul class="title">
+                                        <li><a href="#">@ETUDEOFFICIAL</a></li>
+                                    </ul>
+                                    <ul class="sub">
+                                        <li><a href="#">Instagram</a></li>
+                                        <li><a href="#">Tiktok</a></li>
+                                    </ul>
+                                    <ul class="title">
+                                        <li><a href="./store.html">STORE</a></li>
+                                    </ul>
+                                    <ul class="sub">
+                                        <li><a href="#">온라인</a></li>
+                                        <li><a href="#">오프라인</a></li>
+                                    </ul>
+                                    <ul class="title">
+                                        <li><a href="#">CONTACT</a></li>
+                                    </ul>
+                                    <ul class="sub">
+                                        <li><a href="#">공지사항</a></li>
+                                        <li><a href="#">FAQ</a></li>
+                                        <li><a href="#">고객문의</a></li>
+                                    </ul>
+                                    <ul class="title">
+                                        <li><a href="./login.html">MEMBERSHIP</a></li>
+                                    </ul>
+                                    <ul class="sub">
+                                        <li><a href="#">핑크멤버십</a></li>
+                                        <li><a href="#">뷰티포인트</a></li>
+                                    </ul>
+                                    <ul class="title">
+                                        <li><a href="#">뽀오얀</a></li>
+                                    </ul>
+                                    <ul class="sub">
+                                        <li><a href="#">About뽀오얀</a></li>
+                                        <li><a href="#">제품 보기</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- 데스크탑 버전에만 있는 footer nav 부분 //end// -->
+                </div>
+                <div class="second">
+                    <a href="#">공지사항</a>
+                    <a href="#">이용약관</a>
+                    <a href="#">전자공고</a>
+                    <a href="#">개인정보처리방침</a>
+                    <a href="#">영상정보처리기기 운영·관리 방침</a>
+                </div>
+                <div class="info">
+                    <div class="info_left">
+                        <div class="container">
+                            <a href="#" class="footer_logo"><img src="./images/logo/img_logo2023-1.png" alt="에뛰드하우스"></a>
+                            <div class="info_txt">
+                                <p>(주)에뛰드 대표이사 : 이수연</p>
+                                <p>개인정보보호책임자 : 이수연</p>
+                                <p>사업자등록번호 : 135-81-05033</p>
+                                <p>주소 : 서울특별시 용산구 한강대로 100 (한강로 2가)</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="info_right">
+                        <p><strong>CONTACT</strong></p>
+                        <div class="info_guide">
+                            <div class="icon tel_number">
+                                <div class="icon_img">
+                                    <img src="./images/icon/phone.png" alt="">
+                                </div>
+                                <div class="right">
+                                    <span>제품 문의 : 080-022-2285</span>
+                                    <span>고객 문의 : 1544-5418</span>
+                                </div>
+                            </div>
+                            <div class="icon time">
+                                <div class="icon_img">
+                                    <img src="./images/icon/time.png" alt="">
+                                </div>
+                                <div class="right">
+                                    <span>운영 시간 : 평일 13:00 ~ 17:00 (주말 및 공휴일 휴무)</span>
+                                    <span>점심 시간 : 12:00 ~ 13:00</span>
+                                </div>
+                            </div>
+                            <div class="icon email">
+                                <div class="icon_img">
+                                    <img src="./images/icon/mail.png" alt="">
+                                </div>
+                                <div class="right">
+                                    <span>대표 이메일 : etude_master@etude.co.kr</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer_copy">
+                    <p><em>&copy; ETUDE CORPORATION. ALL RIGHTS RESERVED.</em></p>
+                </div>
+                <div class="sns_icon">
+                    <a href="#"><img src="./images/icon/instagram.png" alt="에뛰드하우스 인스타그램 바로가기"></a>
+                    <a href="#"><img src="./images/icon/facebook.png" alt="에뛰드하우스 페이스북 바로가기"></a>
+                    <a href="#"><img src="./images/icon/youtube.png" alt="에뛰드하우스 유튜브 바로가기"></a>
+                    <a href="#"><img src="./images/icon/tik-tok.png" alt="에뛰드하우스 틱톡 바로가기"></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="./script/common.js"></script>
+    <script src="./script/footer.js"></script>
