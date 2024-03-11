32281af43592ddd90a4be4d92f602c413ac2c8db
eunji <dmswl7954@gmail.com>
2024-03-11 22:27:09 +0900

    join 정규표현식 진행중

--- script/join.js
+++ script/join.js
@@ -12,3 +12,54 @@ document.querySelectorAll('a').forEach(link => {
     });
 });
 /* ---------------------- 구분선 -------------------------- */  
+const userId = document.querySelector('#user_id')
+const userPw = document.querySelector('#user_pw')
+const userPwChk = document.querySelector('#user_pw_ck')
+const useMail = document.querySelector('#user_name')
+const errorMassage = document.querySelectorAll('.error_massage'); // 에러 메시지 출력 요소
+
+const validate = () => {
+// 아이디 유효성 검사
+const idRegex = /^[a-z0-9]{4,16}$/;
+if (!idRegex.test(userId.value)) {
+    errorMassage.innerHTML = '아이디는 영문 소문자와 숫자 4~16자로 입력해야 합니다.';
+    return false;
+}
+
+// 비밀번호 유효성 검사
+const pwRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{10,16}$/;
+if (!pwRegex.test(userPw.value)) {
+    errorMassage.innerHTML = '비밀번호는 영문 대소문자, 숫자, 특수문자 중 2가지 이상 조합하여 10~16자로 입력해야 합니다.';
+    return false;
+}
+
+// 비밀번호 확인
+if (userPw.value !== userPwChk.value) {
+    errorMassage.innerHTML = '비밀번호가 일치하지 않습니다.';
+    return false;
+}
+
+// 이메일 유효성 검사
+const emailRegex = /^[\w-]+@[\w-]+(\.[w-]+)+$/;
+if (!emailRegex.test(userEmail.value)) {
+    errorMassage.innerHTML = '이메일 주소 형식이 올바르지 않습니다.';
+    return false;
+}
+
+// 모든 유효성 검사를 통과하면 true 반환
+return true;
+};
+
+const submitForm = (event) => {
+event.preventDefault();
+
+if (!validate()) {
+    return;
+}
+
+// 유효성 검사를 통과하면 회원가입 처리
+// ...
+};
+
+// document.querySelector('form').addEventListener('submit', submitForm);
+/* ---------------------- 구분선 -------------------------- */  
--- script/login.js
+++ script/login.js
@@ -1,4 +1,4 @@
-/* ---------------------- 구분선 -------------------------- */      
+/* ---------------------- 상단으로 올라가는 것 막기 -------------------------- */      
 // 링크 설정되지 않은 a 태그 클릭시 상단으로 올라가는 것 막기
 document.querySelectorAll('a').forEach(link => {
     link.addEventListener('click', (event) => {
@@ -11,5 +11,24 @@ document.querySelectorAll('a').forEach(link => {
         }
     });
 });
-/* ---------------------- 구분선 -------------------------- */  
+/* ---------------------- 로그인 에러 메세지 -------------------------- */  
+const login_user_id = document.querySelector('#user_id')
+const login_user_pw = document.querySelector('#user_pw')
+const login_btn = document.querySelector('.login_btn')
+const error_message_id = document.querySelector('.id_error')
+const error_message_pw = document.querySelector('.pw_error')
+console.log(login_user_id,login_user_pw,error_message_id,login_btn,error_message_pw)
 
+login_btn.addEventListener('click',()=>{
+    if(login_user_id.value == ''){
+        error_message_id.innerHTML =  '아이디를 입력해 주세요' 
+    }else if(login_user_pw.value == ''){ 
+        // error_message_id.innerHTML =  ''
+        error_message_pw.innerHTML =  '비밀번호를 입력해 주세요'
+    }else {
+        error_message_id.innerHTML =  ``
+        error_message_pw.innerHTML =  `아이디 또는 비밀번호를 잘못 입력했습니다.<br>입력 내용을 다시 확인해주세요.`
+    }
+})
+
+/* ---------------------- 구분선 -------------------------- */  
